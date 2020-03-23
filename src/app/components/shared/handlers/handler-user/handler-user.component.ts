49c3131d0f501b0c20699463322ee146bb297dc6
Miguel Bogota <miguelbogota.rico@outlook.com>
2020-03-23 11:21:04 -0500

    Component flickering fixed
    The previous version had an issue with the routing where the data flicked when the user navigate in the page. Now the user is store in the localstoraga to save time loading the user and building the components. Additional the firestorage was added to be cached with pwa.

--- src/app/components/shared/handlers/handler-user/handler-user.component.ts
+++ src/app/components/shared/handlers/handler-user/handler-user.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit, ViewChild, ViewContainerRef, ComponentFactoryResolver, ComponentFactory } from '@angular/core';
+import {
+  Component, OnInit, AfterViewInit, Input, ChangeDetectorRef,
+  ComponentFactoryResolver, ViewChild, ViewContainerRef, ComponentFactory
+} from '@angular/core';
 import { IUser } from 'src/app/core/models/user.model';
 import { ActivatedRoute, Router } from '@angular/router';
 
 // Designs
 import { BasicComponent } from '../../../designs/basic/basic.component';
 import { UserService } from 'src/app/core/services/user.service';
+import { startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-handler-user',
   templateUrl: './handler-user.component.html',
   styleUrls: ['./handler-user.component.scss']
 })
-export class HandlerUserComponent implements OnInit {
+export class HandlerUserComponent implements OnInit, AfterViewInit {
 
   uid: string = null; // Property for the id of the user
-  user: IUser; // User in the url
   showLoading = true; // Property to know if the page is loading
+  CACHE_KEY = 'userLoaded'; // Nane where the user will be stored
 
   // Insert the component in this container
   @ViewChild('content', { read: ViewContainerRef }) content: ViewContainerRef;
@@ -23,6 +27,7 @@ export class HandlerUserComponent implements OnInit {
   // Constructor with dependency injection
   constructor(
     private resolver: ComponentFactoryResolver,
+    private cdRef: ChangeDetectorRef,
     private router: Router,
     private route: ActivatedRoute,
     private userService: UserService
@@ -34,25 +39,47 @@ export class HandlerUserComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    // Validate if there's any user in local
+    if (localStorage[this.CACHE_KEY] != null) {
+      // Validate is the same user than the url
+      if (JSON.parse(localStorage[this.CACHE_KEY]).uid === this.uid) {
+        // Show cuser from local
+        this.buildComponent(JSON.parse(localStorage[this.CACHE_KEY]) as IUser);
+      }
+      // If user is other one search in db
+      else { this.storeUser(); }
+    }
+    // If there's no user search in db
+    else { this.storeUser(); }
+    // Stop animation
+    this.showLoading = false;
+    // Left at the end to not detect changed but until the end
+    this.cdRef.detectChanges();
+  }
+
+  // Function creates the design component
+  buildComponent(user: IUser): void {
+    const factory = this.getDesign(user.settings.design); // Component to show the user
+    const componentRef = this.content.createComponent(factory); // Create component
+    componentRef.instance.user = user; // Pass the params to the component
+  }
+
+  // Function refresh the user stored
+  storeUser(): void {
     // Get the user from the route uid
     this.userService.getUser(this.uid).subscribe((user: IUser) => {
       // Validate if user exist
       if (user) {
-        this.user = user; // Store the user
-        // Load design choosen by the user
-        const factory = this.getDesign(this.user.settings.design); // Component to show the user
-        // Stop animation
-        this.showLoading = false;
-        // Load component if is not loading
-        if (!this.showLoading) {
-          const componentRef = this.content.createComponent(factory);
-          componentRef.instance.user = this.user;
-        }
+        // Store currect user waching
+        localStorage[this.CACHE_KEY] = JSON.stringify(user);
+        // Build component into the view
+        this.buildComponent(JSON.parse(localStorage[this.CACHE_KEY]) as IUser);
       }
       // If it does not exist send to error
-      else {
-        this.router.navigate(['/error'], { skipLocationChange: true });
-      }
+      else { this.router.navigate(['/error'], { skipLocationChange: true });  }
     });
   }
 
