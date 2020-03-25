e6cbdc49dced542970a121fa579493f01b24f457
Miguel Bogota <miguelbogota.rico@outlook.com>
2020-03-25 13:22:33 -0500

    Cached and versioning
    It was dded an fuction to get new updates in the app asking the user if they would like to update it to refresh it. Aditional some information was cahed to avoid loading the data again like the font and the icons. Some changes were made in the view in order to test the functionallity of the new versioning function.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor() { }
+  constructor(private swUpdate: SwUpdate) {}
+
+  ngOnInit(): void {
+    this.reloadCache();
+  }
+
+  // This function check if an update is available and refresh the page
+  reloadCache() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        // Message to the user to update
+        if (confirm('¡Nueva version disponible! ¿Le gustaría actualizar?')) {
+          // Refresh page if there's any update
+          window.location.reload();
+        }
+      });
+    }
+  }
 
 }
