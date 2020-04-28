e1fdb16f96e9dc8dd02d522e11c1f9a53033d734
Miguel Bogota <miguelbogota.rico@outlook.com>
2020-04-28 14:44:41 -0500

    This fixes #4 and fixes other issues discovered while fixing the component
    This issue was fixed by creating a class open-modal in the body that can be use later from a modal. It only adds the overflow as hidden and that prevents the scroll movement when the menu is open.
    
    Additional another problem was found when you click the outside of the open menu to close it will trigger the element behind the shadow. This was also fixed by making the shadow a div and checking for the click in that element to close it. This also was implemented in the pop menu. When you're in the pop menu the scroll won't trigger the close of the button and this is the correct functioning for the component, might change in the future but for now it works great.

--- src/app/modules/controls/menu/menu.component.ts
+++ src/app/modules/controls/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, HostListener, ElementRef, HostBinding, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, HostListener, ElementRef, HostBinding, Output, EventEmitter, ViewChild } from '@angular/core';
 import { ILink } from 'src/app/shared/models/link.model';
 
 @Component({
@@ -9,7 +9,7 @@ import { ILink } from 'src/app/shared/models/link.model';
 export class MenuComponent implements OnInit {
 
   // Menu's properties
-  @Input() menuState: boolean; // Property for the state of the menu if open or closed
+  menuState = false; // Property for the state of the menu if open or closed
   @Input() menuType: 'fixed' | 'pop' = 'fixed'; // Property will show the menu. Fixed in a part of the screen or as a pop up
   @Input() menuPosition: 'left' | 'right' = 'left'; // Property will show the menu in the left or right of the screen
   @Input() menuTitle: string; // Title for the menu can be enter as a property
@@ -25,17 +25,23 @@ export class MenuComponent implements OnInit {
   @Input() buttonImage = ''; // Image for the button - Will be prioritized over the icon
   // Add style to the host for the visibility
   @HostBinding('class') classes: string;
-
   // Send click event to parent
   @Output() clicked: EventEmitter<MouseEvent> = new EventEmitter();
-
-  // Listener checking if the user clicks outside component to close the menu
-  @HostListener('document:click', ['$event'])
-  clickout(e: any) { if(!this.eRef.nativeElement.contains(e.target) && this.menuState) { this.menuState = false; } }
+  @ViewChild('shadow') shadow: ElementRef;
 
   constructor(
     private eRef: ElementRef
   ) { }
+
+  // Listener checking if the user clicks outside component to close the menu
+  @HostListener('document:click', ['$event'])
+  clickout(e: any) {
+    const clickedOutside = !this.eRef.nativeElement.contains(e.target);
+    const clickedShadow = this.shadow.nativeElement.contains(e.target);
+    if(( clickedOutside || clickedShadow ) && this.menuState) { this.menuState = false; }
+    this.lockScroll(this.menuState);
+
+  }
 
   ngOnInit(): void {
     // Set the menu width to the one passed on the properties
@@ -48,7 +54,19 @@ export class MenuComponent implements OnInit {
 
   // Click event for the links
   sendClickEvent(e: MouseEvent) {
+    this.menuState = false;
     this.clicked.emit(e);
+  }
+
+  // Function will lock the scroll in the background
+  lockScroll(lock: boolean) {
+    // Get the body
+    const body: HTMLElement = document.body as HTMLElement;
+    // Only make changes if the menu is of type fixed
+    if (this.menuType === 'fixed') {
+      if (lock) { body.classList.add('modal-open'); }
+      else { body.classList.remove('modal-open'); }
+    }
   }
 
 }
